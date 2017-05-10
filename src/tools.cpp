31ec469d01b2e8d799193121c4492543b5773aff
Tariq Rafique <mrafique@salesforce.com>
2017-05-09 22:32:18 -0700

    wip

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,26 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+    cout << "Size mis-match";
+    return rmse;
+  }
+  
+  //accumulate squared residuals
+  for(int i=0; i < estimations.size(); ++i){
+    VectorXd diff = (estimations[i] - ground_truth[i]);
+    diff = diff.array() * diff.array();
+    rmse = rmse + diff;
+  }
+  
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  
+  return rmse;
 }
